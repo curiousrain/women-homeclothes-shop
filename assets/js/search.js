46f8c38a4dd301574e9a19144a6b1c6e2cf0c1b4
Nazgul <nazgulchan@gmail.com>
2022-04-16 04:06:50 +0500

    Доделанный страница поиска

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    tooltips: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
